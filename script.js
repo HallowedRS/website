ba11e83285f4a362084f360ea58d8ac43b4b86f3
FozzieOSRS <142063659+FozzieOSRS@users.noreply.github.com>
2023-09-24 14:01:09 -0700

    Upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+//Mobile Menu Functionality
+
+const primaryNav = document.querySelector('.nav-list-ul');
+const navToggle = document.querySelector('.mobile-menu-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+    }
+});
+
+
+//Animate on scroll effect
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+//Hide Nav Bar on scroll
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementsByClassName("navigation").style.transform = translateY(-100);
+//   } else {
+//     document.getElementsByClassName("navigation").style.transform = translateY(0);
+//   }
+// }
