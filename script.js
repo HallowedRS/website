b00903188d53ae0a59634417d17353b49a598c04
FozzieOSRS <142063659+FozzieOSRS@users.noreply.github.com>
2023-10-29 19:27:09 -0700

    Hero Video Optimization
    This push allows the video hero to be **loaded** using javascript only if the screen size exceeds 1000px. It was previously being loaded regardless of if it was being used or not. This should help prevent loading an unused video on mobile and increases mobile speeds.
    
    Mobile hero remains loaded and hidden per usual.

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 const primaryNav = document.querySelector('.nav-list-ul');
 const navToggle = document.querySelector('.mobile-menu-toggle');
 
-var windowWidth = $(window).width();
-
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
 
@@ -32,49 +30,47 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+//Load video only on screen size parameter (hero image loads based on CSS parameter)
 
+var windowWidth = window.innerWidth;
+
+if (windowWidth >1000) {
+
+    var div = document.createElement('video')
+    div.classList.add('desktop-hero')
+
+
+    div.src = './Assets/HallowedSiteHero.mp4'
+    div.muted = true;
+    div.autoplay = true;
+    div.loop = true;
+    div.setAttribute('poster','./Assets/Video Frame.png')
+
+    var box = document.getElementById('header');
+    box.appendChild(div)
+}
+
+//Connors version
 
 // if (windowWidth > 1000) {
-//     // Create video element
-//     const video = document.createElement('video');
-//     video.classList.add("desktop-hero")
-//     // Use local file
-//     // video.src = 'video.mp4';
-
-//     // Use remote file
-//     video.src = '../Assets/Hallowed Site Hero.mp4';
-
-//     video.loop = true;
-//     video.muted = true;
-//     video.autoplay = true;
-    
-
+//     // Check if 'header' element exists
 //     const box = document.getElementById('header');
-
-//     // Include in HTML as child of #box
-//     box.appendChild(video);
-
+//     if (box) {
+//         // Create video element
+//         const video = document.createElement('video');
+//         video.classList.add("desktop-hero");
+        
+//         // Use remote file
+//         video.src = '../Assets/Hallowed Site Hero.mp4';
+        
+//         video.loop = true;
+//         video.muted = true;
+//         video.autoplay = true;
+        
+//         // Include in HTML as child of #box
+//         box.appendChild(video);
+//     }
 // }
-
-if (windowWidth > 1000) {
-    // Check if 'header' element exists
-    const box = document.getElementById('header');
-    if (box) {
-        // Create video element
-        const video = document.createElement('video');
-        video.classList.add("desktop-hero");
-        
-        // Use remote file
-        video.src = '../Assets/Hallowed Site Hero.mp4';
-        
-        video.loop = true;
-        video.muted = true;
-        video.autoplay = true;
-        
-        // Include in HTML as child of #box
-        box.appendChild(video);
-    }
-}
 
 //Hide Nav Bar on scroll
 
